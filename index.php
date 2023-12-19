ea3a5f23cfd119d7aecba3ec32b2134319da85b1
Chahid Hadifi <chahidhadifi@gmail.com>
2023-12-19 22:59:50 +0100

    add index page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fast Food Simulator</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="user-input">
+            <h1>Make an order 🍴</h1>
+            <form method="post">
+                <input type="text" name="name" placeholder="Your name">
+                <input type="number" name="table" placeholder="Your table number" max="10" min="1">
+                <input type="submit" value="submit">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
